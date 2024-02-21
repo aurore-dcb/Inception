7a454f7221083b47cc6ff3c408b41a20c2fa7f9d
aducobu <auroreducobu@gmail.com>
2024-02-21 07:40:36 -0500

    maridb: script

--- srcs/requirements/mariadb/conf/mdb_script.sh
+++ srcs/requirements/mariadb/conf/mdb_script.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+SQL_DATABASE=database
+SQL_USER=mysql
+SQL_PASSWORD=1234
+SQL_ROOT_PASSWORD=42Paris
 service mariadb start;
-sleep 1
-mariadb -e "CREATE DATABASE IF NOT EXISTS \'${SQL_DATABASE}\';"
-mariadb -e "CREATE USER IF NOT EXISTS \'${SQL_USER}\'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON \'${SQL_DATABASE}\'.* TO \'${SQL_USER}\'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mariadb -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
-exec mysqld_safe
+sleep 5
+
+mysql -u root -p"${SQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mariadb -u root -p"${SQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+mysqladmin -u root --password=${SQL_ROOT_PASSWORD} shutdown
+
+#sleep 5
+mysqld_safe
+#exec mysqld -u root
+
+echo "Successfully created"
